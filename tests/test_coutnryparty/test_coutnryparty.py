45451c484dfda871b81a5f44ba62cceedaec9a88
Pavlo Shtohryn <pavlo.stogryn@gmail.com>
2024-01-06 23:25:32 +0200

    update test_save_counter_party

--- tests/test_coutnryparty/test_coutnryparty.py
+++ tests/test_coutnryparty/test_coutnryparty.py
@@ -7,10 +7,13 @@ from utils.counterparty import Counterparty
 class TestCounterparty:
     @pytest.mark.counterparty
     @pytest.mark.positive
-    def test_save_counter_party(self):
+    @pytest.mark.parametrize('first_name', 'middle_name', 'last_name', 'phone', 'email', 'counterparty_type', 'c_property', [
+        ("Іван", "Іванович", "Іванов", "380997979789", "test@i.com","PrivatePerson", "Recipient")
+    ])
+    def test_save_counter_party(self, first_name, middle_name, last_name, phone, email, counterparty_type, c_property):
         """
         Test
         """
-        response_post = Counterparty.save_counter_party("Іван", "Іванович", "Іванов", "380997979789", "test@i.com",
-                                                        "PrivatePerson", "Recipient")
+        response_post = Counterparty.save_counter_party(first_name, middle_name, last_name, phone, email,
+                                                        counterparty_type, c_property)
         assert response_post.json()['success'] is True
