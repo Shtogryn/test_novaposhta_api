5cc2d5a67f3edf80b5557d7767d06c12ec541882
Pavlo Shtohryn <pavlo.stogryn@gmail.com>
2024-01-06 22:41:46 +0200

    update test_save_counter_party

--- tests/test_coutnryparty/test_coutnryparty.py
+++ tests/test_coutnryparty/test_coutnryparty.py
@@ -8,7 +8,7 @@ class TestCounterparty:
     @pytest.mark.positive
     def test_save_counter_party(self):
         """
-        Test
+        Test save new country party
         """
         response_post = Counterparty.save_counter_party("Іван", "Іванович", "Іванов", "380997979789", "test@i.com",
                                                         "PrivatePerson", "Recipient")
